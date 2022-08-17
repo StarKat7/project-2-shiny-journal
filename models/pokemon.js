05c9e871ac55399aadbe02996c409304724a205b
starkat7 <kaelbe@hotmail.com>
2022-08-17 14:58:34 -0700

    Add Game section for Pokemon on both lists

--- models/pokemon.js
+++ models/pokemon.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const pokemonSchema = new mongoose.Schema({
     name: String,
     //image: String,
+    game: String,
     location: String,
     huntMethods: String,
     notes: String
